b72c5abc28e0f50e846764188cb4b7bd1ebce3ba
Ali Abri <silkenpy@gmail.com>
2018-12-26 15:43:07 +0330

    first commit

---
+++ src/main/java/ir/rkr/goldpan/kafka/KafkaConnector.kt
@@ -0,0 +1,99 @@
+package ir.rkr.goldpan.kafka
+
+import com.typesafe.config.Config
+import ir.rkr.goldpan.utils.GoldPanMetrics
+import mu.KotlinLogging
+import java.util.*
+import org.apache.kafka.clients.consumer.KafkaConsumer
+import org.apache.kafka.clients.producer.Callback
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.clients.producer.RecordMetadata
+import java.net.InetAddress
+import java.time.Duration
+import kotlin.collections.HashMap
+
+
+class KafkaConnector(val topicName: String, config: Config, val goldPanMetrics: GoldPanMetrics) {
+
+    val consumer: KafkaConsumer<ByteArray, ByteArray>
+    val producer: KafkaProducer<ByteArray, ByteArray>
+    private val logger = KotlinLogging.logger {}
+
+    init {
+
+        val hostName = InetAddress.getLocalHost().hostName
+
+        val producerCfg = Properties()
+        config.getObject("kafka.producer").forEach({ x, y -> println("$x --> $y"); producerCfg.put(x, y.unwrapped()) })
+        producerCfg.put("key.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
+        producerCfg.put("value.serializer", "org.apache.kafka.common.serialization.ByteArraySerializer")
+        producer = KafkaProducer(producerCfg)
+
+        val consumerCfg = Properties()
+        config.getObject("kafka.consumer").forEach({ x, y -> println("kafka config $x --> $y"); consumerCfg.put(x, y.unwrapped()) })
+        consumerCfg.put("key.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer")
+        consumerCfg.put("value.deserializer", "org.apache.kafka.common.serialization.ByteArrayDeserializer")
+//        consumerCfg.put("group.id", "${hostName}${System.currentTimeMillis()}")
+//        consumerCfg.put("auto.offset.reset", "earliest")
+//        consumerCfg.put("enable.auto.commit", "false")
+        consumer = KafkaConsumer(consumerCfg)
+
+        Thread.sleep(100)
+    }
+
+    fun get(): HashMap<String, String> {
+
+//        goldPanMetrics.MarkKafkaGetCall(1)
+        val msg = HashMap<String, String>()
+
+        return try {
+
+            consumer.subscribe(Collections.singletonList(topicName))
+            val res = consumer.poll(Duration.ofMillis(700))
+            res.records(topicName).forEach { it -> msg[String(it.key())] = String(it.value())   }
+            goldPanMetrics.MarkKafkaGetRecords(msg.size.toLong())
+            msg
+
+        } catch (e: java.lang.Exception) {
+            goldPanMetrics.MarkKafkaGetFail(1)
+            msg
+        }
+    }
+
+
+    fun put(key: String, value: String): Boolean {
+
+        goldPanMetrics.MarkKafkaPutCall(1)
+
+        try {
+            val res = producer.send(ProducerRecord(topicName, key.toByteArray(), value.toByteArray()), object : Callback {
+                override fun onCompletion(p0: RecordMetadata?, p1: Exception?) {
+
+                    if (p1 != null) {
+
+                        logger.error { "key=$key value=$value" }
+                    }
+                }
+            })
+
+            if (res.isDone) {
+                goldPanMetrics.MarkKafkaPutFail(1)
+                return false
+            }
+
+            goldPanMetrics.MarkKafkaPutRecords(1)
+            return true
+
+        } catch (e: Exception) {
+            goldPanMetrics.MarkKafkaPutFail(1)
+            logger.error { "Error in try catch" }
+            return false
+        }
+    }
+
+    fun commit() {
+        consumer.commitAsync()
+    }
+
+}
---
+++ src/main/java/ir/rkr/goldpan/utils/GoldPanMetrics.kt
@@ -0,0 +1,91 @@
+package ir.rkr.goldpan.utils
+
+
+import com.codahale.metrics.Gauge
+import com.codahale.metrics.Meter
+import com.codahale.metrics.MetricRegistry
+import java.util.function.Supplier
+
+
+data class MeterPojo(val count: Long,
+                     val rate: Double,
+                     val oneMinuteRate: Double,
+                     val fiveMinuteRate: Double,
+                     val fifteenMinuteRate: Double)
+
+data class ServerInfo(val gauges: Map<String, Any>, val meters: Map<String, MeterPojo>)
+
+class GoldPanMetrics {
+
+    val metricRegistry = MetricRegistry()
+
+    val NettyRequests = metricRegistry.meter("nettyRequests")
+
+    val KafkaGetCall = metricRegistry.meter("kafkaGetCall")
+    val KafkaGetFail = metricRegistry.meter("kafkaGetFail")
+    val KafkaGetRecords = metricRegistry.meter("kafkaGetRecords")
+
+    val KafkaPutCall = metricRegistry.meter("KafkaPutCall")
+    val KafkaPutFail = metricRegistry.meter("KafkaPutFail")
+    val KafkaPutRecords = metricRegistry.meter("kafkaPutRecords")
+
+    val CaffeineSetCall = metricRegistry.meter("CaffeineSetCall")
+    val CaffeineSetWithTTL = metricRegistry.meter("CaffeineSetWithTTL")
+    val CaffeineSetWithoutTTL = metricRegistry.meter("CaffeineSetWithoutTTL")
+    val CaffeineSetElapsedTTL = metricRegistry.meter("CaffeineSetElapsedTTL")
+    val CaffeineSetSuccess = metricRegistry.meter("CaffeineSetSuccess")
+    val CaffeineSetFail = metricRegistry.meter("CaffeineSetFail")
+
+    val CaffeineGetCall = metricRegistry.meter("CaffeineGetCall")
+    val CaffeineGetAvailable = metricRegistry.meter("CaffeineGetAvailable")
+    val CaffeineGetNotAvailable = metricRegistry.meter("CaffeineGetNotAvailable")
+    val CaffeineGetFail = metricRegistry.meter("CaffeineGetFail")
+
+    val CaffeineDelSuccess = metricRegistry.meter("CaffeineDelSuccess")
+    val CaffeineDelFail = metricRegistry.meter("CaffeineDelFail")
+
+    val CaffeineExpireSuccess = metricRegistry.meter("CaffeineExpireSuccess")
+    val CaffeineExpireFail = metricRegistry.meter("CaffeineExpireFail")
+
+    fun MarkNettyRequests(l: Long = 1) = NettyRequests.mark(l)
+
+    fun MarkKafkaGetFail(l: Long = 1) = KafkaGetFail.mark(l)
+    fun MarkKafkaGetRecords(l: Long = 1) = KafkaGetRecords.mark(l)
+
+    fun MarkKafkaPutCall(l: Long = 1) = KafkaPutCall.mark(l)
+    fun MarkKafkaPutFail(l: Long = 1) = KafkaPutFail.mark(l)
+    fun MarkKafkaPutRecords(l: Long = 1) = KafkaPutRecords.mark(l)
+
+    fun MarkCaffeineSetCall(l: Long = 1) = CaffeineSetCall.mark(l)
+    fun MarkCaffeineSetWithTTL(l: Long = 1) = CaffeineSetWithTTL.mark(l)
+    fun MarkCaffeineSetWithoutTTL(l: Long = 1) = CaffeineSetWithoutTTL.mark(l)
+    fun MarkCaffeineSetElapsedTTL(l: Long = 1) = CaffeineSetElapsedTTL.mark(l)
+    fun MarkCaffeineSetSuccess(l: Long = 1) = CaffeineSetSuccess.mark(l)
+    fun MarkCaffeineSetFail(l: Long = 1) = CaffeineSetFail.mark(l)
+
+    fun MarkCaffeineGetCall(l: Long = 1) = CaffeineGetCall.mark(l)
+    fun MarkCaffeineGetAvailable(l: Long = 1) = CaffeineGetAvailable.mark(l)
+    fun MarkCaffeineGetNotAvailable(l: Long = 1) = CaffeineGetNotAvailable.mark(l)
+    fun MarkCaffeineGetFail(l: Long = 1) = CaffeineGetFail.mark(l)
+
+    fun MarkCaffeineDelSuccess(l: Long = 1) = CaffeineDelSuccess.mark(l)
+    fun MarkCaffeineDelFail(l: Long = 1) = CaffeineDelFail.mark(l)
+
+    fun MarkCaffeineExpireSuccess(l: Long = 1) =CaffeineExpireSuccess.mark(l)
+    fun MarkCaffeineExpireFail(l: Long = 1) =CaffeineExpireFail.mark(l)
+
+
+    fun <T> addGauge(name: String, supplier: Supplier<T>) = metricRegistry.register(name, Gauge<T> { supplier.get() })
+
+    private fun sortMetersByCount(meters: Map<String, Meter>) =
+            meters.toList().sortedBy { it.second.count }.reversed()
+                    .map { Pair(it.first, it.second.toPojo()) }.toMap()
+
+    private fun Meter.toPojo() = MeterPojo(count, meanRate, oneMinuteRate, fiveMinuteRate, fifteenMinuteRate)
+
+    fun getInfo() = ServerInfo(metricRegistry.gauges.mapValues { it.value.value },
+            sortMetersByCount(metricRegistry.meters))
+
+
+}
+
